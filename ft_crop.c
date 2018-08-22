e5f76ef31e6cf52dd40d45caa95251800157e593
Caelan Reedy <rreedy@e1z3r6p6.42.us.org>
2018-08-21 22:40:11 -0700

    dfasdf

--- ft_crop.c
+++ ft_crop.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/16 15:55:00 by rreedy            #+#    #+#             */
-/*   Updated: 2018/08/21 17:52:10 by rreedy           ###   ########.fr       */
+/*   Updated: 2018/08/21 22:28:41 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 char	*precision(char *s, char type, int precision)
 {
-//	printf("Precision value: %i\n", precision);
-	if (type == 's' && ft_strlen(s) >= (size_t)precision)
+	if (type == 's')
 	{
-		s[precision] = '\0';
+		if (ft_strlen(s) >= (size_t)precision)
+			s[precision] = '\0';
 		return (s);
 	}
 	if (precision == 0 && ft_strequ(s, "0"))
@@ -26,18 +26,17 @@ char	*precision(char *s, char type, int precision)
 		return (s);
 	if (s[0] == '-' && (size_t)precision < ft_strlen(s))
 		return (s);
-//	printf("Before Prec Shift: \"%s\"\n", s);
-	s = ft_shift(&s, precision - ft_strlen(s), precision);
-//	printf("After Prec shift: \"%s\"\n", s);
+	if (s[0] != '-')
+		s = ft_shift(&s, precision - ft_strlen(s), precision);
+	else
+		s = ft_shift(&s, precision - ft_strlen(s) + 1, precision + 1);
 	s = addzeros(s);
-//	printf("After Zeros shift: \"%s\"\n", s);
 	return (s);
 }
 
 char	*addzeros(char *s)
 {
 	int		i;
-
 	i = 0;
 	while (s && s[i] == ' ')
 	{
@@ -63,22 +62,16 @@ char	*ft_shift(char **s, int in, size_t size)
 	char	*str;
 	char	*cur;
 
-//	printf("index: %d\n", in);
-//	printf("size: %zu\n", size);
 	str = ft_strinit(' ', size);
 	cur = *s;
-//	printf("cur: \"%s\"\n", cur);
-//	printf("str: \"%s\"\n", str);
 	if (!str)
 		return (0);
 	while (cur && *cur && size)
 	{
 		str[in] = *cur;
-//		printf("str in shift: \"%s\"\n", str);
 		++cur;
 		++in;
 		--size;
-//		printf("size in shift: %zu\n", size);
 	}
 	ft_strdel(s);
 	return (str);
@@ -111,7 +104,7 @@ char	*flag(char *s, char *flags, char type)
 	return (s);
 }
 
-char	*fill_flags(char *fmt, char *s, char type)
+char	*fill_flags(char *s, char *fmt, char type)
 {
 	char	*flags;
 	int		i;
@@ -145,41 +138,22 @@ char	*crop(char *s, char *fmt)
 	char	*flags;
 	char	type;
 	int		p;
-	int		prec;
 	int		w;
 
-
 	type = *to_type(fmt);
-	flags = fill_flags(fmt, s, type);
-//	printf("type: \"%c\"\n", type);
-//	printf("flags: \"%s\"\n", flags);
-
+	flags = fill_flags(s, fmt, type);
 	while (fmt && (!ft_isalnum(*fmt) || *fmt == '0') && *fmt != '.')
 		++fmt;
 	w = (ft_isdigit(*fmt)) ? ft_atoi(fmt) : 0;
 	while (fmt && !ft_isalpha(*fmt) && *fmt != '.')
 		++fmt;
-	prec = (*fmt++ == '.') ? 1 : 0;
-	p = (prec) ? ft_atoi(fmt) : ft_strlen(s);
-
-//	printf("Width Value: \"%d\"\n", w);
-//	printf("Precision Value: \"%d\"\n", p);
-
-//	printf("Before Crop: \"%s\"\n", s);
-	
-	s = precision(s, type, p);
-//	printf("Precision: \"%s\"\n", s);
-	
+	p = (*fmt++ == '.') ? ft_atoi(fmt) : -1;
+	if (p >= 0)	
+		s = precision(s, type, p);
 	if (ft_strchr(flags, '#') || ft_strchr(flags, '+') || ft_strchr(flags, ' '))
 		s = flag(s, flags, type);
-//	printf("Flags: \"%s\"\n", s);
-	
 	s = width(s, flags, w);
-//	printf("Width: \"%s\"\n", s);
-
-	if (ft_strchr(flags, '0') && !prec)
+	if (ft_strchr(flags, '0') && p == -1)
 		s = addzeros(s);
-//	printf("Zeros: \"%s\"\n", s);
-
 	return (s);
 }
